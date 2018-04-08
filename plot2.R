e2bf1a703c2d3e74c94d9483392fd6e64761f1b4
roach231428 <roach231428@users.noreply.github.com>
2018-04-08 23:37:33 +0800

    plot 2 finished
    plot 2 finished

---
+++ plot2.R
@@ -0,0 +1,13 @@
+library(lattice)
+library(grid)
+# Let string input not be transformed into factor format
+options(stringsAsFactors = FALSE)
+
+rawData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+# Extract data whose Date is between 2007/02/02 and 2007/02/02
+needData <- rawData[as.Date(rawData[,1], "%d/%m/%Y") >= as.Date("2007/02/01") & 
+                      as.Date(rawData[,1], "%d/%m/%Y") <= as.Date("2007/02/02"), ]
+png("plot2.png")
+time = strptime(paste(needData[,1], needData[,2]), format  ="%d/%m/%Y %H:%M:%S")
+plot(time, needData$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
